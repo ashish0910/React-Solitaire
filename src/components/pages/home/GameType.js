6035c228d1da75776c46a19e4921fa0b7b53a632
ashish0910 <ashish09101998@gmail.com>
2020-11-05 15:09:31 +0530

    Klondike logic complete

--- src/components/pages/home/GameType.js
+++ src/components/pages/home/GameType.js
@@ -6,7 +6,14 @@ function GameType(props) {
   return (
     <div className="gametype">
       <Link to={props.type}>
-        <p className="gametype__link"> {props.type} </p>
+        <div>
+          <div className="gametype__image">
+            <img
+              src={`https://raw.githubusercontent.com/ashish0910/React-Solitaire/main/src/assets/four-suit.png${props.type}.png`}
+            ></img>
+          </div>
+          <div className="gametype__link"> {props.type} </div>
+        </div>
       </Link>
     </div>
   );
