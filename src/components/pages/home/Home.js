aa1f20583fe2d4d5800f85e6d0fe5b95df3afc3a
ashish0910 <ashish09101998@gmail.com>
2020-11-07 21:54:04 +0530

    CSS Fixes

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -2,17 +2,21 @@ import React from "react";
 import Header from "./Header";
 import GameTypeWrapper from "./GameTypeWrapper";
 import GameType from "./GameType";
+import ScreenSizeWarn from "../../ScreenSizeWarn";
 
 function Home() {
   return (
-    <div className="home">
-      <Header />
-      <GameTypeWrapper>
-        <GameType type="one-suite" heading="One Suit" />
-        <GameType type="four-suite" heading="Four Suit" />
-        <GameType type="klondike" heading="Klondike" />
-      </GameTypeWrapper>
-    </div>
+    <>
+      <ScreenSizeWarn />
+      <div className="home">
+        <Header />
+        <GameTypeWrapper>
+          <GameType type="one-suite" heading="One Suit" />
+          <GameType type="four-suite" heading="Four Suit" />
+          <GameType type="klondike" heading="Klondike" />
+        </GameTypeWrapper>
+      </div>
+    </>
   );
 }
 
